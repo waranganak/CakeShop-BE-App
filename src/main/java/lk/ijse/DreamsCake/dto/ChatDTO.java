1ff6f3d1bd21b81df7d634367ee9edb9b6fe7976
kauminiwarangana <kauminiwarangana143@gmail.com>
2026-09-24 11:36:36 +0530

    Add ChatDTO for chatbot

---
+++ src/main/java/lk/ijse/DreamsCake/dto/ChatDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.DreamsCake.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatDTO {
+    private String message;
+    private String reply;
+}
